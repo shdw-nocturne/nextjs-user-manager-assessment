cf3e29c0753f72b82dab502ca284bd9f853e8d47
Jaycon46 <jaycon@itsjobvious.com>
2025-11-28 03:55:26 +0800

    feat: add error catch in get users

--- app/users/actions/get-users.ts
+++ app/users/actions/get-users.ts
@@ -6,8 +6,13 @@ import path from "path";
 import { UserTypes } from "../types/user.types";
 
 export default async function getUsers(): Promise<UserTypes[]> {
-	const filePath = path.join(process.cwd(), "data", "users.json");
-	const fileData = await fs.readFile(filePath, "utf-8");
-	const users = JSON.parse(fileData);
-	return users.sort((a: UserTypes, b: UserTypes) => b.id - a.id);
+	try {
+		const filePath = path.join(process.cwd(), "data", "users.json");
+		const fileData = await fs.readFile(filePath, "utf-8");
+		const users = JSON.parse(fileData);
+		return users.sort((a: UserTypes, b: UserTypes) => b.id - a.id);
+	} catch (error) {
+		console.error("Error reading users file:", error);
+		return [];
+	}
 }
